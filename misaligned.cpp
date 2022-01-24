362fee87d34a3ea079d3b9e4096602ff97eedbed
Harshini Narasimha <91076579+Harshini-Narasimha@users.noreply.github.com>
2022-01-24 21:56:45 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
 #include <assert.h>
 
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int getColorPairNumber(int majorColorIndex, int minorColorIndex)
+{
+    return majorColorIndex * 5 + minorColorIndex;
+}
+
+std::string getColorPairString(int majorColorIndex, int minorColorIndex)
+{
+    std::string majorColorName(majorColor[majorColorIndex]);
+    std::string minorColorName(minorColor[majorColorIndex]);
+    return majorColorName.append(" | ").append(minorColorName);
+}
+
+void printColorCode(int majorColorIndex, int minorColorIndex)
+{
+   std::cout << getColorPairNumber(majorColorIndex,minorColorIndex) << " | " <<  getColorPairString(majorColorIndex,majorColorIndex) << "\n";
+}
+
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
+
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-        }
+
+            printColorCode(i,j);
+       }
     }
     return i * j;
 }
 
 int main() {
+
     int result = printColorMap();
+
     assert(result == 25);
-    assert(result == 24);
+    assert(getColorPairNumber(0,4) == 4);
+    assert(getColorPairString(0,0) == "White | Blue");
+    assert(getColorPairString(1,0) == "Red | Blue");
+
     std::cout << "All is well (maybe!)\n";
+
     return 0;
+
 }
